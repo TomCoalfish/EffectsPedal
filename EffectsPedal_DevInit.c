4d9106ae0a003690c51d73af8a58a9e2cb36d2f7
KBako <kieranzkbako@gmail.com>
2021-10-23 12:42:04 -0700

    light edits
    made some light edits

--- EffectsPedal_DevInit.c
+++ EffectsPedal_DevInit.c
@@ -8,6 +8,9 @@
 //
 // Author:              David Romalo
 //
+// Modified By:         Matthew Peeters, A01014378
+//                      Kieran Bako, A01028276
+//
 // Date:                19Oct2021
 
 #include <Headers/F2837xD_device.h>
@@ -29,22 +32,22 @@ EALLOW;
     //---------------------------------------------------------------
     // INITIALIZE A-D
     //---------------------------------------------------------------
-    CpuSysRegs.PCLKCR13.bit.ADC_A = 1; //enable A-D clock for ADC-A
-    AdcaRegs.ADCCTL2.bit.PRESCALE = 0xf;
-    AdcaRegs.ADCCTL1.bit.ADCPWDNZ = 1;
+    CpuSysRegs.PCLKCR13.bit.ADC_B = 1; //enable A-D clock for ADC-D
+    AdcbRegs.ADCCTL2.bit.PRESCALE = 0xf;
+    AdcbRegs.ADCCTL1.bit.ADCPWDNZ = 1;
 
     //generate INT pulse on end of conversion:
-    AdcaRegs.ADCCTL1.bit.INTPULSEPOS = 1;
+    AdcbRegs.ADCCTL1.bit.INTPULSEPOS = 1;
 
     //wait 1 ms after power-up before using the ADC:
     DelayUs(1000);
 
-    AnalogSubsysRegs.TSNSCTL.bit.ENABLE = 1; //connect temp sensor to ADCIN13 on ADC-A
+    //AnalogSubsysRegs.TSNSCTL.bit.ENABLE = 1; //connect temp sensor to ADCIN13 on ADC-A
 
-    AdcaRegs.ADCSOC0CTL.bit.TRIGSEL = 2; //trigger source = CPU1 Timer 1
-    AdcaRegs.ADCSOC0CTL.bit.CHSEL = 13; //set SOC0 to sample A5
-    AdcaRegs.ADCSOC0CTL.bit.ACQPS = 139; //set SOC0 window to 139 SYSCLK cycles
-    AdcaRegs.ADCINTSEL1N2.bit.INT1SEL = 0; //connect interrupt ADCINT1 to EOC0
-    AdcaRegs.ADCINTSEL1N2.bit.INT1E = 1; //enable interrupt ADCINT1*/
+    AdcbRegs.ADCSOC0CTL.bit.TRIGSEL = 2; //trigger source = CPU1 Timer 1
+    AdcbRegs.ADCSOC0CTL.bit.CHSEL = 13; //set SOC0 to sample A5
+    AdcbRegs.ADCSOC0CTL.bit.ACQPS = 139; //set SOC0 window to 139 SYSCLK cycles
+    AdcbRegs.ADCINTSEL1N2.bit.INT1SEL = 0; //connect interrupt ADCINT1 to EOC0
+    AdcbRegs.ADCINTSEL1N2.bit.INT1E = 1; //enable interrupt ADCINT1*/
 EDIS;
 }
